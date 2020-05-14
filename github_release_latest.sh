486591a6d13032bea8e2f44ebfff57a0e24acbae
Jakub Bednar <jakub.bednar@gmail.com>
2020-05-14 16:08:08 +0200

    add repository to path

--- github_release_latest.sh
+++ github_release_latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl https://api.github.com/repos/influxdata/influxdb-client-java/releases/latest | jq ". + {repository_id: \"$1\"}"
+curl https://api.github.com/repos/influxdata/$1/releases/latest | jq ". + {repository_id: \"$1\"}"
